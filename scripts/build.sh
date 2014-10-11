54d54a6a7f5f9f30c3d888c8b9f607c6db71c459
Niels Søholm <nm@9la.dk>
2014-10-12 07:38:42 +0800

    Added very simple build script that just makes a tar-archieve of the application files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Procedures needed to deploy application in a production setting.
-# Certain files/configurations should be different in a production
-# environment as opposed to a development environment.
+# Certain files/configurations should be different or excluded in
+# a production environment as opposed to a development environment.
 #
 # Niels Søholm (2014-10-11)
+
+mkdir build
+mkdir build/package
+
+cp index.js build/package/index.js
+cp package.json build/package/package.json
+
+tar jcvf build/nourriture-release.tar.bz2 build/package
