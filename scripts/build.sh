bf6182030f63c305029ab3cf8984aad68dc89bd1
Niels Søholm <nm@9la.dk>
2014-10-12 11:39:33 +0800

    The build script now appends the build_number to the filename of the resulting archieve

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,4 @@ mkdir build/package
 cp index.js build/package/index.js
 cp package.json build/package/package.json
 
-tar jcvf build/nourriture-release.tar.bz2 build/package
+tar jcvf build/nourriture-0.1.${BUILD_NUMBER}.tar.bz2 build/package
