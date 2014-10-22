2b860a807eaeb9f6b5b3d4c41eaf4a7784b6dba6
Pavel <pprochazka72@gmail.com>
2014-10-22 21:00:40 +0800

    Pre-created modules

--- modules/company_module.js
+++ modules/company_module.js
@@ -1,3 +1,26 @@
 /**
- * Created by remoteusrr on 22/10/14.
+ * Created by Pavel Prochazka on 22/10/14.
+ * Module for all company specific API calls.
  */
+
+function createCompanyProfile(req, res, next){
+    console.log('Create company requested');
+}
+
+function readCompanyProfile(req, res, next){
+    console.log('Read company requested');
+}
+
+function updateCompanyProfile(req, res, next){
+    console.log('Update company requested');
+}
+
+function deleteCompanyProfile(req, res, next){
+    console.log('Delete company requested');
+}
+
+//MODULE - functions that I want to expose as a module
+exports.createCompany   = createCompanyProfile;
+exports.readCompany     = readCompanyProfile;
+exports.updateCompany   = updateCompanyProfile;
+exports.deleteCompany   = deleteCompanyProfile;
---
+++ modules/recipe_module.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Pavel Prochazka on 22/10/14.
+ * Module for all recipe specific API calls.
+ *
+ * NOTE: a recipe consists of combination of several products/ingredients.
+ */
