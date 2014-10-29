1e38e556d14ffe5debb5b6084185bc6ffad446b3
Pavel <pprochazka72@gmail.com>
2014-10-30 01:17:30 +0800

    Refactoring

--- modules/company_module.js
+++ modules/company_module.js
@@ -6,85 +6,86 @@
 var restify = require('restify');
 var saveModule = require('save')('company');    //TODO: module to stub fake companies, should be deleted once DB in place
 
-function createCompanyProfile(req, res, next){
-    console.log('Create company requested');
+module.exports = function (server, models) {
 
-    if (req.params.name === undefined) {
-        return next(new restify.InvalidArgumentError('Company name attribute missing'));
-    }
-    else if(req.params.address === undefined){
-        return next(new restify.InvalidArgumentError('Company address attribute missing'));
-    }
+    server.post('/company/', function(req, res, next) {
+        console.log('Create company requested');
 
-    saveModule.create({ name: req.params.name , address: req.params.address}, function (error, company) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        if (req.params.name === undefined) {
+            return next(new restify.InvalidArgumentError('Company name attribute missing'));
         }
-        res.send(201, company) //the '201 Created' HTTP response code + created company
-        next();
-    })
-}
-
-function readCompanyProfile(req, res, next){
-    console.log('Read company requested');
-
-    saveModule.findOne({ _id: req.params.id }, function (error, company) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        else if (req.params.address === undefined) {
+            return next(new restify.InvalidArgumentError('Company address attribute missing'));
         }
 
-        if (company) {
-            res.send(company)
+        saveModule.create({name: req.params.name, address: req.params.address}, function (error, company) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+            res.send(201, company) //the '201 Created' HTTP response code + created company
             next();
-        } else {    //'Save' may provide an error, or undefined for the user variable if they don't exist
-            res.send(404)
+        })
+    });
+
+    server.get('/company/:id', function (req, res, next) {
+        console.log('Read company requested');
+
+        saveModule.findOne({_id: req.params.id}, function (error, company) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+
+            if (company) {
+                res.send(company)
+                next();
+            } else {    //'Save' may provide an error, or undefined for the user variable if they don't exist
+                res.send(404)
+                next();
+            }
+        })
+    });
+
+    server.get('/company', function (req, res, next) {
+        console.log('Read all companies requested');
+
+        saveModule.find({}, function (error, companies) {
+            res.send(companies);
             next();
+        })
+    });
+
+    server.put('/company/:id', function (req, res, next) {
+        console.log('Update company requested');
+
+        if (req.params.name === undefined) {
+            return next(new restify.InvalidArgumentError('Company name attribute missing'))
         }
-    })
-}
-
-function readAllCompanyProfiles(req, res, next){
-    console.log('Read all companies requested');
-
-    saveModule.find({}, function (error, companies) {
-        res.send(companies);
-        next();
-    })
-}
-
-function updateCompanyProfile(req, res, next){
-    console.log('Update company requested');
-
-    if (req.params.name === undefined) {
-        return next(new restify.InvalidArgumentError('Company name attribute missing'))
-    }
-    else if(req.params.address === undefined){
-        return next(new restify.InvalidArgumentError('Company address attribute missing'));
-    }
-
-    saveModule.update({ _id: req.params.id, name: req.params.name , address: req.params.address}, function (error, user) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        else if (req.params.address === undefined) {
+            return next(new restify.InvalidArgumentError('Company address attribute missing'));
         }
-        res.send();
-        next();
-    })
-}   //TODO: why does not work to test with WebStorm REST plugin???
 
-function deleteCompanyProfile(req, res, next){
-    console.log('Delete company requested');
+        saveModule.update({
+            _id: req.params.id,
+            name: req.params.name,
+            address: req.params.address
+        }, function (error, user) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+            res.send();
+            next();
+        })
+    });   //TODO: why does not work to test with WebStorm REST plugin???
 
-    saveModule.delete(req.params.id, function (error, company) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-        }
-        res.send();
-        next();
-    })
-}
+    server.del('/company/:id', function (req, res, next) {
+        console.log('Delete company requested');
 
-exports.createCompany   = createCompanyProfile;
-exports.readCompany     = readCompanyProfile;
-exports.readAllCompanies= readAllCompanyProfiles;
-exports.updateCompany   = updateCompanyProfile;
-exports.deleteCompany   = deleteCompanyProfile;
+        saveModule.delete(req.params.id, function (error, company) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+            res.send();
+            next();
+        })
+    });
+};
