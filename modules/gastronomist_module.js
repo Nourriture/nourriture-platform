475470986225435d0f3351fc74c05ca0f70d1e73
Pavel <pprochazka72@gmail.com>
2014-10-30 01:18:44 +0800

    Refactoring

--- modules/gastronomist_module.js
+++ modules/gastronomist_module.js
@@ -5,85 +5,86 @@
 var restify = require('restify');
 var saveModule = require('save')('gastronomist');
 
-function createGastronomistProfile(req, res, next){
-    console.log('Create gastronomist requested');
+module.exports = function (server, models) {
 
-    if (req.params.name === undefined) {
-        return next(new restify.InvalidArgumentError('Gastronomist name attribute missing'));
-    }
-    else if(req.params.address === undefined){
-        return next(new restify.InvalidArgumentError('Gastronomist address attribute missing'));
-    }
+    server.post('/gastronomist/', function (req, res, next) {
+        console.log('Create gastronomist requested');
 
-    saveModule.create({ name: req.params.name , address: req.params.address}, function (error, gastronomist) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        if (req.params.name === undefined) {
+            return next(new restify.InvalidArgumentError('Gastronomist name attribute missing'));
         }
-        res.send(201, gastronomist) //the '201 Created' HTTP response code + created company
-        next();
-    })
-}
-
-function readGastronmistProfile(req, res, next){
-    console.log('Read gastronomist requested');
-
-    saveModule.findOne({ _id: req.params.id }, function (error, gastronomist) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        else if (req.params.address === undefined) {
+            return next(new restify.InvalidArgumentError('Gastronomist address attribute missing'));
         }
 
-        if (gastronomist) {
-            res.send(gastronomist)
+        saveModule.create({name: req.params.name, address: req.params.address}, function (error, gastronomist) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+            res.send(201, gastronomist) //the '201 Created' HTTP response code + created company
             next();
-        } else {
-            res.send(404)
+        })
+    });
+
+    server.get('/gastronomist/:id', function (req, res, next) {
+        console.log('Read gastronomist requested');
+
+        saveModule.findOne({_id: req.params.id}, function (error, gastronomist) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+
+            if (gastronomist) {
+                res.send(gastronomist)
+                next();
+            } else {
+                res.send(404)
+                next();
+            }
+        })
+    });
+
+    server.get('/gastronomist', function (req, res, next) {
+        console.log('Read all gastronomist requested');
+
+        saveModule.find({}, function (error, gastronomist) {
+            res.send(gastronomist);
             next();
+        })
+    });
+
+    server.put('/gastronomist/:id', function (req, res, next) {
+        console.log('Update gastronomist requested');
+
+        if (req.params.name === undefined) {
+            return next(new restify.InvalidArgumentError('Gastronomist name attribute missing'))
         }
-    })
-}
-
-function readAllGastronomistProfiles(req, res, next){
-    console.log('Read all gastronomist requested');
-
-    saveModule.find({}, function (error, gastronomist) {
-        res.send(gastronomist);
-        next();
-    })
-}
-
-function updateGastronomistProfile(req, res, next){
-    console.log('Update gastronomist requested');
-
-    if (req.params.name === undefined) {
-        return next(new restify.InvalidArgumentError('Gastronomist name attribute missing'))
-    }
-    else if(req.params.address === undefined){
-        return next(new restify.InvalidArgumentError('Gastronomist address attribute missing'));
-    }
-
-    saveModule.update({ _id: req.params.id, name: req.params.name , address: req.params.address}, function (error, user) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        else if (req.params.address === undefined) {
+            return next(new restify.InvalidArgumentError('Gastronomist address attribute missing'));
         }
-        res.send();
-        next();
-    })
+
+        saveModule.update({
+            _id: req.params.id,
+            name: req.params.name,
+            address: req.params.address
+        }, function (error, user) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+            res.send();
+            next();
+        })
+    });
+
+    server.del('/gastronomist/:id', function (req, res, next) {
+        console.log('Delete gastronomist requested');
+
+        saveModule.delete(req.params.id, function (error, company) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+            }
+            res.send();
+            next();
+        })
+    });
 }
-
-function deleteGastronomistProfile(req, res, next){
-    console.log('Delete gastronomist requested');
-
-    saveModule.delete(req.params.id, function (error, company) {
-        if (error){
-            return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-        }
-        res.send();
-        next();
-    })
-}
-
-exports.createGastronomist   = createGastronomistProfile;
-exports.readGastronomist     = readGastronmistProfile;
-exports.readAllGastronomist  = readAllGastronomistProfiles;
-exports.updateGastronomist   = updateGastronomistProfile;
-exports.deleteGastronomist   = deleteGastronomistProfile;
