6877522e3970b47080dfe768c329b246cf4df9de
gonod <vincent.gonod@epitech.eu>
2014-11-15 23:15:39 +0800

    recipe update

--- modules/recipe_module.js
+++ modules/recipe_module.js
@@ -11,52 +11,26 @@ module.exports = function (server, models) {
     server.post('/recipe/', function (req, res, next) {
         console.log('Insert Recipe requested');
 
-        var newRecipe = new models.Recipe(req.body);
-
-        newRecipe.save(function (err){
-
-        if(!err) {
-               res.send(req.body);
-               next();
-           } else {
-            if (req.params.name === undefined) {
+        if (req.params.name === undefined) {
             return next(new restify.InvalidArgumentError('Recipe name attribute missing'));
         }
-            else (req.params.components === undefined) {
+        else if (req.params.components === undefined) {
             return next(new restify.InvalidArgumentError('Recipe components attribute missing')); //"Components" it's a list of components for the recipe
-        }            
-           }
+        }
 
-        })
-        
-
-/*        saveModule.create({name: req.params.name, components: req.params.components}, function (error, recipe) {
+        saveModule.create({name: req.params.name, components: req.params.components}, function (error, recipe) {
             if (error) {
                 return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
             }
             res.send(201, recipe) //the '201 Created' HTTP response code + created recipe
             next();
-        })*/
+        })
     });
 
     server.get('/recipe/:id', function (req, res, next) {
         console.log('Select recipe by id requested');
 
-        models.Recipe.find({ "_id":req.params.id }, function(err, Recipe) {
-            if(!err) {
-                    res.send(recipe);
-                    next();
-                } else {
-                    next(new restify.ResourceNotFoundError("No recipes found with the given id"));
-                }
-            } else {
-                console.error("Failed to query database for recipe:", err);
-                next(new restify.InternalError("Failed to get recipe due to an unexpected internal error"));
-            }
-        });
-
-
-/*        saveModule.findOne({_id: req.params.id}, function (error, recipe) {
+        saveModule.findOne({_id: req.params.id}, function (error, recipe) {
             if (error) {
                 return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
             }
@@ -68,27 +42,13 @@ module.exports = function (server, models) {
                 res.send(404)
                 next();
             }
-        })*/
+        })
     });
 
     server.get('/recipe/:name', function (req, res, next) {
         console.log('Select recipe by name requested');
 
-        models.Recipe.find({ username:req.params.username }, { "_id":0 }, function(err, recipe) {
-            if(!err) {
-                if(recipe.length != 0) {
-                    res.send(recipe);
-                    next();
-                } else {
-                    next(new restify.ResourceNotFoundError("No recipes found with the given name"));
-                }
-            } else {
-                console.error("Failed to query database for recipe:", err);
-                next(new restify.InternalError("Failed to get recipe due to an unexpected internal error"));
-            }
-        });
-
-        /*saveModule.findOne({_name: req.params.name}, function (error, recipe) {
+        saveModule.findOne({_name: req.params.name}, function (error, recipe) {
             if (error) {
                 return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
             }
@@ -100,13 +60,13 @@ module.exports = function (server, models) {
                 res.send(404)
                 next();
             }
-        })*/
+        })
     });
 
     server.get('/recipe', function (req, res, next) {
         console.log('Select all recipes requested');
 
-        models.Recipe.find({}, function (error, recipe) {
+        saveModule.find({}, function (error, recipe) {
             res.send(companies);
             next();
         })
@@ -115,35 +75,13 @@ module.exports = function (server, models) {
     server.put('/recipe/:id', function (req, res, next) {
         console.log('Update recipe requested');
 
-        if (!req.body) {
-            next(new restify.InvalidContentError("No recipe submitted for update"));
-            return;
+        if (req.params.name === undefined) {
+            return next(new restify.InvalidArgumentError('Recipe name attribute missing'))
+        }
+        else if (req.params.components === undefined) {
+            return next(new restify.InvalidArgumentError('Recipe components attribute missing'));
         }
 
-        models.Recipe.find({ "_id":req.params.id }, function(err, Recipe) {
-            if(!err) {
-                newRecipe.save(function (err){
-
-                if(!err) {
-                    res.send(req.body);
-                    next();
-                } else {
-                    if (req.params.name === undefined) {
-                        return next(new restify.InvalidArgumentError('Recipe name attribute missing'));
-                    }
-                    else (req.params.components === undefined) {
-                        return next(new restify.InvalidArgumentError('Recipe components attribute missing')); //"Components" it's a list of components for the recipe
-                    }            
-                }
-                } else {
-                    next(new restify.ResourceNotFoundError("No recipes found with the given id"));
-                }
-            } else {
-                console.error("Failed to query database for recipe:", err);
-                next(new restify.InternalError("Failed to get recipe due to an unexpected internal error"));
-            }
-        });
-        /*
         saveModule.update({
             _id: req.params.id,
             name: req.params.name,
@@ -154,24 +92,18 @@ module.exports = function (server, models) {
             }
             res.send();
             next();
-        })*/
+        })
     });  //TODO: why does not work to test with WebStorm REST plugin???
 
     server.del('/recipe/:id', function (req, res, next) {
-        console.log('Delete recipe requested');
+        console.log('Delete company requested');
 
-        models.Consumer.findOneAndRemove({ "_id":req.params.id} }, function(err, deletedRecipe) {
-            if(!err) {
-                if(deletedRecipe) {
-                    res.send(deletedRecipe);
-                    next();
-                } else {
-                    next(new restify.ResourceNotFoundError("No recipe found with the given id"));
-                }
-            } else {
-                console.error("Failed to delete recipe from database:", err);
-                next(new restify.InternalError("Failed to recipe due to an unexpected internal error"));
+        saveModule.delete(req.params.id, function (error, company) {
+            if (error) {
+                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
             }
-        });
+            res.send();
+            next();
+        })
     });
 };
