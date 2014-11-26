738643b5e66c045427fb969b8f4bed9e8bcf216e
Pavel <pprochazka72@gmail.com>
2014-11-26 12:21:47 +0800

    Create and Get based on ID TESTED

--- modules/company_module.js
+++ modules/company_module.js
@@ -13,7 +13,7 @@ module.exports = function (server, models) {
     server.post('/company/', function(req, res, next) {
         console.log('Create company requested');
 
-        var newCompany = new models.Company(req.body);
+        var newCompany = new models.Company(req.body);  //req.body has to be POSTed in JSON format!!!
 
         newCompany.save(function (err) {
             if(!err) {
@@ -21,30 +21,13 @@ module.exports = function (server, models) {
                 next();
             } else {
                 if(err.name == "ValidationError") {
-                    next(new restify.InvalidContentError(err.toString()));
+                    next(new restify.InvalidContentError(err.toString()));  //Restify takes care of HTTP error handling
                 } else {
                     console.error("Failed to insert company into database:", err);
-                    next(new restify.InternalError("Failed to insert company due to an unexpected internal error"));
+                    next(new restify.InternalError("Failed to insert company due to an unexpected internal error"));    //Restify takes care of HTTP error handling
                 }
             }
         });
-
-        /*
-        Using SAVE module
-        if (req.params.name === undefined) {
-            return next(new restify.InvalidArgumentError('Company name attribute missing'));
-        }
-        else if (req.params.address === undefined) {
-            return next(new restify.InvalidArgumentError('Company address attribute missing'));
-        }
-
-        saveModule.create({name: req.params.name, address: req.params.address}, function (error, company) {
-            if (error) {
-                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-            }
-            res.send(201, company) //the '201 Created' HTTP response code + created company
-            next();
-        })*/
     });
 
     //Read a company based on ID
@@ -64,20 +47,6 @@ module.exports = function (server, models) {
                 next(new restify.InternalError("Failed to read company due to an unexpected internal error"));
             }
         });
-
-        /*saveModule.findOne({_id: req.params.id}, function (error, company) {
-            if (error) {
-                return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-            }
-
-            if (company) {
-                res.send(company)
-                next();
-            } else {    //'Save' may provide an error, or undefined for the user variable if they don't exist
-                res.send(404)
-                next();
-            }
-        })*/
     });
 
     //Read all companies //TODO: finish me
