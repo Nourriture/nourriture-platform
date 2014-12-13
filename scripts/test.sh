55d30887ad948a1face991618b9f147b91e2327b
Pavel <pprochazka72@gmail.com>
2014-12-13 17:37:26 +0800

    Working setup

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,32 +3,12 @@
 #
 # Niels Søholm (2014-10-12)
 
-# TODO: remove me
-BUILD_NUMBER=321
+# Adding "&" to place a job into the background (like opening a new tab)
+node server.js --connection-string "mongodb://localhost:27017/nourriture-app-test" &
 
-# Package name (name of outputted archieve)
-PK_NAME=nourriture-0.1.${BUILD_NUMBER}
+npm install
 
-# Clean up any old build
-rm -r build
+mocha
 
-# Prepare build output folders
-mkdir build
-mkdir build/$PK_NAME
-
-# Copy source files
-cp server.js build/$PK_NAME/server.js
-cp -r modules build/$PK_NAME/modules
-cp -r utilities build/$PK_NAME/utilities
-cp -r models build/$PK_NAME/models
-cp -r test build/$PK_NAME/test
-
-# Inject build number (from Jenkins environment variable) into package.json
-sed package.json -e "s/\(\"version\":\s\"[0-9]*.[0-9]*.\)[0-9]\"/\1$BUILD_NUMBER\"/g" > build/$PK_NAME/package.json
-
-# Replace the "connection-string" to test database
-sed modules/config_module.js -e "s/\(\"connection-string\":\s\"mongodb://localhost:27017/nourriture-app-test\"/g" > build/$PK_NAME/modules/config_module.js
-
-#npm install
-
-#mocha
+# For local debug purposes (kill running NodeJS server)
+killall node
