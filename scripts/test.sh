f7cd310f9aaafacfb7e0d88064843d56d86579e6
Pavel <pprochazka72@gmail.com>
2014-12-13 13:09:42 +0800

    Init setup (in progress)

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,3 +4,24 @@
 # Niels Søholm (2014-10-12)
 
 # TODO: Implement this when we know more about which test framework to use etc.
+BUILD_NUMBER=321
+
+# Package name (name of outputted archieve)
+PK_NAME=nourriture-0.1.${BUILD_NUMBER}
+
+# Clean up any old build
+rm -r build
+
+# Prepare build output folders
+mkdir build
+mkdir build/$PK_NAME
+
+# Copy source files
+cp server.js build/$PK_NAME/server.js
+cp -r modules build/$PK_NAME/modules
+cp -r utilities build/$PK_NAME/utilities
+cp -r models build/$PK_NAME/models
+cp -r test build/$PK_NAME/test
+
+# Inject build number (from Jenkins environment variable) into package.json
+sed package.json -e "s/\(\"version\":\s\"[0-9]*.[0-9]*.\)[0-9]\"/\1$BUILD_NUMBER\"/g" > build/$PK_NAME/package.json
