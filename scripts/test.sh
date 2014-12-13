f95954608f99a7de2f284d36f95c1e7d8c61d54d
Pavel <pprochazka72@gmail.com>
2014-12-13 18:11:43 +0800

    ENVIRONMENTAL VARIABLE

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 # Procedures to run our test framework and output results
 #
-# Niels Søholm (2014-10-12)
+# Pavel Prochazka (2014-12-13)
 
-# Adding "&" to place a job into the background (like opening a new tab)
-node server.js --connection-string "mongodb://localhost:27017/nourriture-app-test" &
+# Set an ENVIRONMENT VARIABLE, which is accessible for all started processes
+export dbcon="mongodb://localhost:27017/nourriture-app-test"
+
+# Replace the "connection-string" inside NodeJS config with the ENVIRONMENT VARIABLE
+# Adding "&" to place a job into the background (like opening a new tab), so that the "npm" and "mocha" don't wait
+node server.js --connection-string $dbcon &
 
 npm install
 
 mocha
 
-# For local debug purposes (kill running NodeJS server)
+# For local debug purposes (kill running NodeJS server) - Jenkins will take care of this
 # killall node
