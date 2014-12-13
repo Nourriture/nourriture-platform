dd591485d06309e5f53fe619ab6dfbb07959051c
Pavel <pprochazka72@gmail.com>
2014-12-13 15:43:49 +0800

    Code to replace "connection-string"

--- scripts/test.sh
+++ scripts/test.sh
@@ -24,5 +24,11 @@ cp -r models build/$PK_NAME/models
 cp -r test build/$PK_NAME/test
 
 # Inject build number (from Jenkins environment variable) into package.json
-# TODO: pass in test DB string!
 sed package.json -e "s/\(\"version\":\s\"[0-9]*.[0-9]*.\)[0-9]\"/\1$BUILD_NUMBER\"/g" > build/$PK_NAME/package.json
+
+# Replace the "connection-string" to test database
+sed modules/config_module.js -e "s/\(\"connection-string\":\s\"mongodb://localhost:27017/nourriture-app-test\"/g" > build/$PK_NAME/modules/config_module.js
+
+#npm install
+
+#mocha
